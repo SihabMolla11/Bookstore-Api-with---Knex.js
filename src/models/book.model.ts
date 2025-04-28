84c54bd0ee538e84b726cfa89fb14936d91716ba
SihabMolla11 <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>
2025-04-29 00:58:30 +0600

    get book details

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -14,7 +14,38 @@ export const getAllBooks = async (title: string) => {
 };
 
 export const getBookById = async (id: number) => {
-  return db('books').where('id', id).first();
+  const book = await db('books')
+    .select(
+      'books.id',
+      'books.title',
+      'books.description',
+      'books.published_date',
+      'books.created_at',
+      'authors.id as author_id',
+      'authors.name as author_name',
+      'authors.bio as author_bio',
+      'authors.birthdate as author_birthdate',
+    )
+    .leftJoin('authors', 'books.author_id', 'authors.id')
+    .where('books.id', id)
+    .first();
+
+  if (book) {
+    return {
+      id: book.id,
+      title: book.title,
+      description: book.description,
+      published_date: book.published_date,
+      created_at: book.created_at,
+      information: {
+        id: book.author_id,
+        name: book.author_name,
+        bio: book.author_bio,
+        birthdate: book.author_birthdate,
+      },
+    };
+  }
+  return null;
 };
 
 export const createBook = async (book: Omit<BookType, 'id' | 'created_at' | 'updated_at'>) => {
